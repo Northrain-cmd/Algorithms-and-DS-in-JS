5d739fd64efe5cdd490a9e4789212790cb4a1436
Northraincmd <59730055+Northrain-cmd@users.noreply.github.com>
2021-08-16 07:28:08 +0000

    Completed merge sort

--- mergeSort.js
+++ mergeSort.js
@@ -1,26 +1,36 @@
 // Write a function that uses merge sort to sort an array of numbers
 
 function mergeSort(array) {
-  return array
+  if(array.length <= 1) {
+    return array
+  } 
+  let middle = Math.floor((array.length / 2));
+  let left = array.slice(0, middle);
+  let right = array.slice(middle);
+  return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(arr1, arr2) {
   result = [];
   let [right, left] = [0, 0];
-  while(arr1[left] && arr2[right]) {
-    // console.log(arr1[left], arr2[right])
+  while(left < arr1.length && right < arr2.length) {
     if(arr1[left] > arr2[right]) {
       result.push(arr2[right]);
       right += 1;
-    } else if(arr1[left] < arr2[right]) {
+    } else {
       result.push(arr1[left]);
       left += 1;
     }
   }
-  //console.log(arr1[left], arr2[right])
-  result.push(arr1[left] || arr2[right]);
+  while(left < arr1.length) {
+    result.push(arr1[left])
+    left += 1
+  }
+  while(right < arr2.length) {
+    result.push(arr2[right])
+    right += 1
+  }
   return result
 }
 
 module.exports = [mergeSort, merge];
-console.log(merge([3],[5]))
--- mergeSort.test.js
+++ mergeSort.test.js
@@ -1,15 +1,17 @@
 const [mergeSort, merge] = require('./mergeSort.js');
 
-xtest('Sorts an array of numbers', () => {
+test('Sorts an array of numbers', () => {
   expect(mergeSort([5,6,3,2,4])).toEqual([2,3,4,5,6]);
 })
 
-xtest('Sorts another array of numbers', () => {
+test('Sorts another array of numbers', () => {
   expect(mergeSort([20,9,80,10,20,5,70])).toEqual([5,9,10,20,20,70,80]);
+  expect(mergeSort([50,40,30,20,10,0,-10,-20,-30,-40,-50])).toEqual([-50,-40,-30,-20,-10,0,10,20,30,40,50]);
 })
 
 test('Merges sorted arrays', () => {
-  expect(merge([3,8],[5,7])).toEqual([3,5,7,8]);;
-  expect(merge([8],[5])).toEqual([5,8]);;
+  expect(merge([3,8],[5,7])).toEqual([3,5,7,8]);
+  expect(merge([8],[5])).toEqual([5,8]);
   expect(merge([8,12,15],[5,13,18])).toEqual([5,8,12,13,15,18]);
+  expect(merge([8,15],[1,7,15])).toEqual([1,7,8,15,15]);
 })
